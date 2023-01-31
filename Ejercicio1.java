b90cc4ac22e8e310b98230980e43a1da4b5d45fb
Álvaro Jiménez Ponce <115788430+alvarojimeenez@users.noreply.github.com>
2023-01-31 10:50:58 +0100

    Actualizando ejercicio 1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -10,24 +10,30 @@ public class Ejercicio1 {
 		que ser distinto de cero) debe devolver un -1.
 
 		 */
-		float a=1, b=4, c=1;
+		int a=1, b=4, c=1;
 		System.out.println(numeroSolucionesEcuacionSegundoGrado(a, b, c));
 	
 	}
 	
 	
-	public static double numeroSolucionesEcuacionSegundoGrado(float a, float b, float c) {
+	public static double numeroSolucionesEcuacionSegundoGrado(int a, int b, int c) {
 		String mensaje = "";
 		// Math.pow(b, 2)-4*a*c>0 tiene 2 soluciones
 		// Math.pow(b, 2)-4*a*c=0 tiene 1 solución
 		// Math.pow(b, 2)-4*a*c tiene 2 soluciones
 		double valorNulo = Math.pow(b, 2)-4*a*c;
-		if (valorNulo<0) {
-			return -1;
+		int resultado = 0;
+		if (a!=0) {
+			if (valorNulo<0) {
+				 resultado = 0;
+			}else if (valorNulo>0) {
+				 resultado = 2;
+			}
+			
+		}else {
+			 resultado = -1;
 		}
-		double x = -b+(Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
-		double x2 = -b-(Math.sqrt(Math.pow(b,2)-(4*a*c)))/(2*a);
-		return x;
+		return resultado;
 	}
 
 }
